1bac2b14ab411dff89c5c1446263f25552bfb0b2
wangwuquan <wwq0806>
2015-05-29 16:39:20 +0800

    Revert "Revert "calculator""
    This reverts commit 493af245e62ce3d8440dbeff7d0005701159d77c.

---
+++ src/zuoye/Calculate.java
@@ -0,0 +1,26 @@
+package zuoye;
+
+public class Calculate {
+	
+	public static double jia( double a, double b)
+	{
+		return a+b;
+	}
+	
+	public static double jian( double a, double b)
+	{
+		return a-b;
+	}
+	
+	public static double cheng( double a, double b)
+	{
+		return a*b;
+	}
+	
+	public static double chu( double a, double b)
+	{
+		return a/b;
+	}
+	
+	
+}
